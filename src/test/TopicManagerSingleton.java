a51089f8eff8409d74abd8b5a887ab709a0520a1
pinati1 <789ori1@gmail.com>
2026-01-14 15:56:08 +0200

    landmark2 for PTM1

--- src/test/TopicManagerSingleton.java
+++ src/test/TopicManagerSingleton.java
@@ -1,10 +1,47 @@
 package test;
 
 
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TopicManagerSingleton {
 
+
+  public static TopicManager get(){return TopicManager.instance;}
+
+
     public static class TopicManager{
+
+        private static final TopicManagerSingleton.TopicManager instance = new TopicManagerSingleton.TopicManager();
+        private ConcurrentHashMap<String, Topic> topics = new ConcurrentHashMap<>() ;
+
+        public static TopicManagerSingleton.TopicManager get(){
+            return TopicManagerSingleton.TopicManager.instance;
+        }
+
+
+        private TopicManager(){}
+
+
+        public Topic getTopic(String name){
+            return topics.computeIfAbsent(name,Topic::new);
+        }
+
+        public Collection<Topic> getTopics(){
+
+            return topics.values();
+        }
+        public void clear(){
+            topics.clear();
+        }
+
+
+
+
     }
 
-    
+
+
 }
