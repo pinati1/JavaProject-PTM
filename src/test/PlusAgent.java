56e08a0b7b508ed79d22896dc6f9d80b0a994919
pinati1 <789ori1@gmail.com>
2026-01-15 11:36:32 +0200

    PlusAgent

--- src/test/PlusAgent.java
+++ src/test/PlusAgent.java
@@ -2,7 +2,53 @@ package test;
 
 import test.TopicManagerSingleton.TopicManager;
 
-public class PlusAgent {
+public class PlusAgent implements Agent {
 
-    
+    private double x= 0,y=0;
+
+    private String inputTopic1,inputTopic2,outputTopic;
+
+    public PlusAgent(String[] subs,String[] pubs){
+
+        this.inputTopic1 = subs[0];
+        this.inputTopic2 = subs[1];
+        this.outputTopic = pubs[0];
+
+        TopicManagerSingleton.get().getTopic(inputTopic1)
+                .subscribe(this);
+
+        TopicManagerSingleton.get().getTopic(inputTopic2)
+                .subscribe(this);
+    }
+    @Override
+    public String getName(){
+            return "PlusAgent";
+    }
+
+    @Override
+    public void reset(){
+        this.x=0;
+        this.y = 0;
+    }
+    @Override
+    public void callback(String topic,Message msg){
+        if(topic.equals(inputTopic1)){
+            x= msg.asDouble;
+        } else if (topic.equals(inputTopic2)) {
+            y= msg.asDouble;
+        }
+
+        if(!Double.isNaN(x)&&!Double.isNaN(y)){
+            TopicManagerSingleton.get().getTopic(outputTopic)
+                    .publish(new Message(x+y));
+        }
+
+    }
+    @Override
+    public void close(){
+        TopicManagerSingleton.get().getTopic(inputTopic1)
+                .unsubscribe(this);
+        TopicManagerSingleton.get().getTopic(inputTopic2)
+                .unsubscribe(this);
+    }
 }
